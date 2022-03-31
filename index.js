4e399da1e3a160fe5785933df87ac5c71bd35f5b
Ivan <ivanzemlyanskiy@gmail.com>
2022-04-01 00:08:55 +0300

    п

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let editButton = document.querySelector('.editbutton');
-let popup = document.querySelector('.popup');
+let popupContainer = document.querySelector('.popup__container');
+
 let popupOpened = document.querySelector('.popup_opened');
 let Close = document.querySelector('.Close');
-let popupKusto = document.querySelector('.popup__kusto');
+let saveButton = document.querySelector('.savebutton');
 let nameTitle = document.querySelector('.name__title');
 let popupText = document.querySelector('.popup__text');
 let nameSubtitle = document.querySelector('.name__subtitle');
-let saveButton = document.querySelector('.savebutton');
-let popupContainer = document.querySelector('.popup__container');
+
+let popupKusto = document.querySelector('.popup__kusto');
+let popup = document.querySelector('.popup');
 
 function Open() {
   popup.classList.add('popup_opened');
@@ -16,19 +18,17 @@ function Open() {
 }
 editButton.addEventListener('click', Open);
 
-
 function Exit() {
   popup.classList.remove('popup_opened');
 }
 Close.addEventListener('click', Exit);
 
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
+  Exit();
   nameTitle.textContent = popupKusto.value;
   nameSubtitle.textContent = popupText.value;
-  Exit();
-}
+ }
 popup.addEventListener('submit', formSubmitHandler);
 
 
